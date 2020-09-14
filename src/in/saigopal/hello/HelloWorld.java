0707de97c29205097e63748706e95391c43c03db
saigopal <saigopalpekal@gmail.com>
2020-09-14 19:10:45 +0530

    Initial Commit: Simple Java Program to print HelloWorld.

---
+++ src/in/saigopal/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package in.saigopal.hello;
+public class HelloWorld {
+   public static void main(String[] args) {
+      System.out.println("Hello World!!!");
+      System.out.println("Welcome to Java");
+      System.out.println("Java follows WORA principle");
+      System.out.println("WORA : Write once and run anywhere");
+   }
+
+}
